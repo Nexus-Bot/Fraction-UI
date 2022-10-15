13eb21ea1e9c7fc6a5e6e145ea75a2e8c4566d05
Prasad Patewar <prasadpatewar39@gmail.com>
2022-10-15 16:04:45 +0530

    Added home page

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import Navbar from "./NavBar";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
